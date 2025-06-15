31159d9ff6cb90e126a04f8a22756a95d3254418
empsgit <empdtj@gmail.com>
2025-06-15 18:44:45 +0500

    restore old dithering

--- app.py
+++ app.py
@@ -472,8 +472,7 @@ def index():
 
 @app.route('/preview', methods=['GET'])
 def preview():
-    return jsonify(rendered_image=rendered_image_data,
-                   success=rendering_complete)
+    return jsonify(rendered_image=rendered_data, success=rendering_complete)
 
 @app.route('/mode/single', methods=['POST'])
 def set_single():
